842650077a6d075407ff14b0599853ec13c22814
Tim Helfensdörfer <tim@visualappeal.de>
2020-03-22 21:31:18 +0100

    Added misc food category

--- database/seeds/FoodGroupSeeder.php
+++ database/seeds/FoodGroupSeeder.php
@@ -43,6 +43,10 @@ class FoodGroupSeeder extends Seeder
                 'name' => 'vegetables',
                 'icon' => 'carrot'
             ],
+            [
+                'name' => 'misc',
+                'icon' => 'candy-cane',
+            ]
         ]);
     }
 }
