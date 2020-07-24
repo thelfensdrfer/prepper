c848c67c96851356a2dff66520400e6091819aaf
Tim Helfensdörfer <tim@visualappeal.de>
2020-07-25 01:01:13 +0200

    Created landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>{{ __('Prepper') }}</h1>
+    <main class="text-center">
+        <h1 class="mt-16"><span class="text-blue-500">{{ __('Be certain') }}</span> {{ __('of what you have in your pantry') }}</h1>
+
+        <p class="text-2xl text-gray-700 mt-8 max-w-lg mx-auto">{{ __('Plan ahead for blackouts or other small and large catastrophes. Know that you are prepared because you have enough food, water, and other necessities for you and your family.') }}</p>
+
+        <p class="mt-16">
+            <a href="{{ route('register') }}" class="btn btn-primary btn-lg">{{ __('Get started') }}</a>
+        </p>
+    </main>
 @endsection
